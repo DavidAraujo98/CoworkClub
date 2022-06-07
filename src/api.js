285f41c3516c43a0f6ae2a13059ad627ad6f4b38
David Araújo <david2araujo5@gmail.com>
2022-06-07 22:14:24 +0100

    Update images

--- src/api.js
+++ src/api.js
@@ -12,12 +12,27 @@ app.get("/spaces", (req, res) => {
        amenities   -> list of amenities selected
     */
     var r = req.query;
-    var response = {
-        search: r.search,
-        local: r.local,
-        type: r.type,
-        amenities: r.amenities.split(" ")
-    }
+    /* 
+        Retrieving matches from database with query parameters...
+    */
+    var response = [
+        {
+            id: "AveiroHub",
+            name: "Aveiro Hub",
+            rating: "4.5",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            price: "30",
+        },
+        {
+            id: "WorkSmart",
+            name: "Work Smart",
+            rating: "4",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            price: "27",
+        },
+    ];
     res.send(response);
 });
 
@@ -32,21 +47,27 @@ app.get("/spaces/:name", (req, res) => {
         This will be fetch every time a space's page is opened.    
     */
     var name = req.params.name;
+    /* 
+        Retrieving matches from database with routing parameters...
+    */
     var response = {
-        name: "AveiroHub",
+        id: "AveiroHub",
+        name: "Aveiro Hub",
+        rating: "4.5",
+        description: "Lorem ipsum dolor sit amet...",
         priceTable: [
             {
                 id: "hotdesk",
                 day: "15",
-                week: "60", 
+                week: "60",
             },
             {
                 id: "office",
                 day: "25",
-                week: "100", 
+                week: "100",
             },
         ],
-    }
+    };
     res.send(response);
 });
 
