7c22396ed16e1c43ab8824e1bde5b7bb3de2db4c
David Araújo <david2araujo5@gmail.com>
2022-06-19 13:31:56 +0100

    Revert "Update"
    This reverts commit 9947538ee0917c36606b4bd6a23d4ee55ee23e2d.

---
+++ src/express-js/api.js
@@ -0,0 +1,214 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/spaces", (req, res) => {
+    /*  QUERY types
+    
+       /spaces?search=Aveiro&type=hotdesk&amenities=coffee+fax
+    
+       search      -> specifies the text inputted in search box
+       type        -> type of item being search
+       amenities   -> list of amenities selected
+    */
+    var r = req.query;
+    /* 
+        Retrieving matches from database with query parameters...
+    */
+    var response = [
+        {
+            id: "AveiroHub",
+            name: "Aveiro Hub",
+            rating: "4.5",
+            parking: true,
+            location:
+                "Rua 25 de Abril, nº2 , Aveiro, 3810-111 Aveiro, Portugal",
+            description:
+                "Premium cowork office in Aveiro (Portuguese Venice).AveiroHub exists in order to change the way people feel about their working week.",
+            price: "30",
+            thumbnail: "assets/office_1.jpg",
+        },
+        {
+            id: "WorkSmart",
+            name: "Work Smart",
+            parking: false,
+            rating: "4",
+            location:
+                "Rua Sargento Mor, nº32 3ºD, Ilhavo, 3910-456 Ilhavo, Portugal",
+            description:
+                "In Ocupa Cowork Aveiro you can get more than just a place to work... you get a community, you get free coffee. Place near the Aveiro river",
+            price: "27",
+            thumbnail: "assets/office_2.jpg",
+        },
+        {
+            id: "OcupaAveiro",
+            name: "Ocupa Aveiro",
+            rating: "4.5",
+            parking: false,
+            location: "Rua da Igrega, nº60 , Anadia, 3780-161 Anadia, Portugal",
+            description:
+                "In Ocupa Cowork Aveiro you can get more than just a place to work... you get a community, you get free coffee. Place near the Aveiro river",
+            price: "20",
+            thumbnail: "assets/office_3.jpg",
+        },
+        {
+            id: "BusinessCenter",
+            name: "Business Center",
+            rating: "4.2",
+            parking: true,
+            location:
+                "Avenida de Maio, nº23, Aveiro, 3910-456 Aveiro, Portugal",
+            description:
+                "In Ocupa Cowork Aveiro you can get more than just a place to work... you get a community, you get free coffee. Place near the Aveiro river",
+            price: "25",
+            thumbnail: "assets/office_4.jpg",
+        },
+        {
+            id: "ClubWork",
+            name: "Club Work",
+            rating: "3.9",
+            parking: false,
+            location:
+                "Avenida de Janeiro, nº23, Aveiro, 3910-456 Aveiro, Portugal",
+            description:
+                "In Ocupa Cowork Aveiro you can get more than just a place to work... you get a community, you get free coffee. Place near the Aveiro river",
+            price: "17",
+            thumbnail: "assets/office_5.jpg",
+        },
+    ];
+    res.send(response);
+});
+
+app.get("/spaces/:name", (req, res) => {
+    /*  Route path
+    
+       /spaces/AveiroHub
+    
+       :name        -> Each space has a unique name id to retrieve info from databases
+        
+        This will be fetch every time a space's page is opened.    
+    */
+    var name = req.params.name;
+    /* 
+        Retrieving matches from database with routing parameters...
+    */
+    var response = {
+        id: "AveiroHub",
+        name: "Aveiro Hub",
+        rating: "4.5",
+        nReview: 134,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Diam maecenas ultricies mi eget mauris. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum. Semper eget duis at tellus at. Augue ut lectus arcu bibendum at. Eget lorem dolor sed viverra ipsum nunc aliquet. Dui sapien eget mi proin sed libero. Gravida cum sociis natoque penatibus et magnis. Luctus accumsan tortor posuere ac ut consequat. Suspendisse in est ante in nibh mauris cursus mattis molestie. Magna fermentum iaculis eu non diam phasellus vestibulum. Bibendum ut tristique et egestas quis ipsum. Ipsum faucibus vitae aliquet nec ullamcorper sit.",
+        location: "Rua 25 de Abril, nº2 , Aveiro, 3810-111 Aveiro, Portugal",
+        accessHours: [
+            "09:00 am - 10:00 pm",
+            "10:00 am - 08:00 pm",
+            "01:00 am - 08:00 pm",
+        ],
+        priceTable: [
+            {
+                id: "hotdesk",
+                name: "Hot Desk",
+                list: [
+                    {
+                        people: "1",
+                        duration: "1 day",
+                        price: "15",
+                        hours: "8 per/day",
+                    },
+                    {
+                        people: "1",
+                        duration: "1 week",
+                        price: "75",
+                        hours: "8 per/day",
+                    },
+                    {
+                        people: "1",
+                        duration: "1 month",
+                        price: "250",
+                        hours: "10 per/day",
+                    },
+                ],
+            },
+            {
+                id: "privoffice",
+                name: "Private Office",
+                list: [
+                    {
+                        people: "1-4",
+                        duration: "1 day",
+                        price: "40",
+                        hours: "8 per/day",
+                    },
+                    {
+                        people: "1-4",
+                        duration: "1 week",
+                        price: "240",
+                        hours: "8 per/day",
+                    },
+                    {
+                        people: "1-4",
+                        duration: "1 month",
+                        price: "810",
+                        hours: "10 per/day",
+                    },
+                ],
+            },
+        ],
+        amenities: ["Free coffee", "Heating", "High Speed Wifi", "Kitchen"],
+    };
+    res.send(response);
+});
+
+app.post("/book/:name", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.get("/profile/:name", (req, res) => {
+    /*  Route path
+    
+       /profile/johndoe
+    
+       :name        -> Each user has a unique name id to retrieve info from databases
+        
+        This will be fetch every time a profile's page is opened.    
+    */
+    var name = req.params.name;
+    /* 
+        Retrieving matches from database with routing parameters...
+    */
+    var response = {
+        id: "johndoe",
+        name: "John Doe",
+        email: "john_doe@email.com",
+        password: "",
+        bookings: [
+            {
+                id: "123",
+                space_id: "WorkSmart",
+                checkin: "22Jun2021-09:00",
+                checkout: "22Jun2021-17:00",
+                type: "hotdesk",
+                price: "35",
+                amenities: [],
+            },
+            {
+                id: "124",
+                space_id: "Worker",
+                checkin: "13Mar2021-09:00",
+                checkout: "15Mar2021-16:00",
+                type: "office",
+                price: "100",
+                amenities: ["coffee", "zoom", "snack"],
+            },
+        ],
+    };
+    res.send(response);
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
---
+++ src/vue-js/src/views/profile.vue
@@ -0,0 +1,295 @@
+<template>
+  <div class="profile-container">
+    <header-logged rootClassName="header-logged-root-class-name3"></header-logged>
+    <div class="profile-hero">
+      <div class="profile-container01">
+        <img
+          alt="image"
+          src="https://play.teleporthq.io/static/svg/default-img.svg"
+          class="profile-image"
+        />
+        <div class="profile-container02">
+          <h1 class="profile-text">John Doe</h1>
+          <div class="profile-container03">
+            <div class="profile-container04">
+              <h3 class="profile-text01">Job type</h3>
+              <span>Software Developer</span>
+            </div>
+            <div class="profile-container05">
+              <h3 class="profile-text03"><span>Preferred office type</span></h3>
+              <span>Hot Desk</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="profile-modal">
+        <h2 class="profile-text06"><span>Personal details</span></h2>
+        <div class="profile-table">
+          <div class="profile-container06">
+            <div class="profile-container07">
+              <h3>Name</h3>
+              <span><span>John William Doe</span></span>
+            </div>
+            <svg viewBox="0 0 865.7188571428571 1024" class="profile-icon">
+              <path
+                d="M207.429 877.714l52-52-134.286-134.286-52 52v61.143h73.143v73.143h61.143zM506.286 347.429c0-7.429-5.143-12.571-12.571-12.571-3.429 0-6.857 1.143-9.714 4l-309.714 309.714c-2.857 2.857-4 6.286-4 9.714 0 7.429 5.143 12.571 12.571 12.571 3.429 0 6.857-1.143 9.714-4l309.714-309.714c2.857-2.857 4-6.286 4-9.714zM475.429 237.714l237.714 237.714-475.429 475.429h-237.714v-237.714zM865.714 292.571c0 19.429-8 38.286-21.143 51.429l-94.857 94.857-237.714-237.714 94.857-94.286c13.143-13.714 32-21.714 51.429-21.714s38.286 8 52 21.714l134.286 133.714c13.143 13.714 21.143 32.571 21.143 52z"
+              ></path>
+            </svg>
+          </div>
+          <div class="profile-container08">
+            <div class="profile-container09">
+              <h3>Email</h3>
+              <span><span>john_doe@email.com</span></span>
+            </div>
+            <svg viewBox="0 0 865.7188571428571 1024" class="profile-icon2">
+              <path
+                d="M207.429 877.714l52-52-134.286-134.286-52 52v61.143h73.143v73.143h61.143zM506.286 347.429c0-7.429-5.143-12.571-12.571-12.571-3.429 0-6.857 1.143-9.714 4l-309.714 309.714c-2.857 2.857-4 6.286-4 9.714 0 7.429 5.143 12.571 12.571 12.571 3.429 0 6.857-1.143 9.714-4l309.714-309.714c2.857-2.857 4-6.286 4-9.714zM475.429 237.714l237.714 237.714-475.429 475.429h-237.714v-237.714zM865.714 292.571c0 19.429-8 38.286-21.143 51.429l-94.857 94.857-237.714-237.714 94.857-94.286c13.143-13.714 32-21.714 51.429-21.714s38.286 8 52 21.714l134.286 133.714c13.143 13.714 21.143 32.571 21.143 52z"
+              ></path>
+            </svg>
+          </div>
+          <div class="profile-container10">
+            <div class="profile-container11">
+              <h3>Password</h3>
+              <span><span>●●●●●●●●●●●●●</span></span>
+            </div>
+            <svg viewBox="0 0 865.7188571428571 1024" class="profile-icon4">
+              <path
+                d="M207.429 877.714l52-52-134.286-134.286-52 52v61.143h73.143v73.143h61.143zM506.286 347.429c0-7.429-5.143-12.571-12.571-12.571-3.429 0-6.857 1.143-9.714 4l-309.714 309.714c-2.857 2.857-4 6.286-4 9.714 0 7.429 5.143 12.571 12.571 12.571 3.429 0 6.857-1.143 9.714-4l309.714-309.714c2.857-2.857 4-6.286 4-9.714zM475.429 237.714l237.714 237.714-475.429 475.429h-237.714v-237.714zM865.714 292.571c0 19.429-8 38.286-21.143 51.429l-94.857 94.857-237.714-237.714 94.857-94.286c13.143-13.714 32-21.714 51.429-21.714s38.286 8 52 21.714l134.286 133.714c13.143 13.714 21.143 32.571 21.143 52z"
+              ></path>
+            </svg>
+          </div>
+          <div class="profile-container12">
+            <div class="profile-container13">
+              <h3>Address</h3>
+              <span><span>Rua 1 de Janeiro, nº 11, Coimbra, Portugal</span></span>
+            </div>
+            <svg viewBox="0 0 865.7188571428571 1024" class="profile-icon6">
+              <path
+                d="M207.429 877.714l52-52-134.286-134.286-52 52v61.143h73.143v73.143h61.143zM506.286 347.429c0-7.429-5.143-12.571-12.571-12.571-3.429 0-6.857 1.143-9.714 4l-309.714 309.714c-2.857 2.857-4 6.286-4 9.714 0 7.429 5.143 12.571 12.571 12.571 3.429 0 6.857-1.143 9.714-4l309.714-309.714c2.857-2.857 4-6.286 4-9.714zM475.429 237.714l237.714 237.714-475.429 475.429h-237.714v-237.714zM865.714 292.571c0 19.429-8 38.286-21.143 51.429l-94.857 94.857-237.714-237.714 94.857-94.286c13.143-13.714 32-21.714 51.429-21.714s38.286 8 52 21.714l134.286 133.714c13.143 13.714 21.143 32.571 21.143 52z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <app-footer rootClassName="footer-root-class-name1"></app-footer>
+  </div>
+</template>
+
+<script>
+import HeaderLogged from '../components/header-logged'
+import AppFooter from '../components/footer'
+
+export default {
+  name: 'Profile',
+  components: {
+    HeaderLogged,
+    AppFooter,
+  },
+  metaInfo: {
+    title: 'Profile - CoworkClub',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Profile - CoworkClub',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.profile-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.profile-hero {
+  width: 100%;
+  display: flex;
+  padding: 48px;
+  max-width: 1400px;
+  margin-top: var(--dl-space-space-sixunits);
+  min-height: 80vh;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.profile-container01 {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.profile-image {
+  width: 200px;
+  height: 100%;
+  object-fit: cover;
+  margin-right: var(--dl-space-space-doubleunit);
+}
+.profile-container02 {
+  height: auto;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.profile-text {
+  color: var(--dl-color-primary-500);
+  font-size: 3em;
+}
+.profile-container03 {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+.profile-container04 {
+  display: flex;
+  align-items: flex-start;
+  margin-right: var(--dl-space-space-doubleunit);
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.profile-text01 {
+  color: var(--dl-color-secondary-500);
+}
+.profile-container05 {
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.profile-text03 {
+  color: var(--dl-color-secondary-500);
+}
+.profile-modal {
+  width: 100%;
+  display: flex;
+  margin-top: var(--dl-space-space-doubleunit);
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.profile-text06 {
+  color: var(--dl-color-secondary-500);
+  width: 100%;
+}
+.profile-table {
+  flex: 0 0 auto;
+  width: 100%;
+  display: flex;
+  box-shadow: 5px 5px 10px 0px #d4d4d4;
+  align-items: flex-start;
+  padding-top: var(--dl-space-space-halfunit);
+  border-color: var(--dl-color-gray-800);
+  border-width: 1px;
+  padding-left: var(--dl-space-space-unit);
+  border-radius: var(--dl-radius-radius-radius40);
+  padding-right: var(--dl-space-space-unit);
+  flex-direction: column;
+  padding-bottom: var(--dl-space-space-halfunit);
+  justify-content: space-between;
+}
+.profile-container06 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.profile-container07 {
+  display: flex;
+  margin-top: var(--dl-space-space-halfunit);
+  align-items: flex-start;
+  margin-bottom: var(--dl-space-space-halfunit);
+  flex-direction: column;
+  justify-content: space-between;
+}
+.profile-icon {
+  width: 24px;
+  height: 24px;
+}
+.profile-container08 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.profile-container09 {
+  display: flex;
+  margin-top: var(--dl-space-space-halfunit);
+  align-items: flex-start;
+  margin-bottom: var(--dl-space-space-halfunit);
+  flex-direction: column;
+  justify-content: space-between;
+}
+.profile-icon2 {
+  width: 24px;
+  height: 24px;
+}
+.profile-container10 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.profile-container11 {
+  display: flex;
+  margin-top: var(--dl-space-space-halfunit);
+  align-items: flex-start;
+  margin-bottom: var(--dl-space-space-halfunit);
+  flex-direction: column;
+  justify-content: space-between;
+}
+.profile-icon4 {
+  width: 24px;
+  height: 24px;
+}
+.profile-container12 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.profile-container13 {
+  display: flex;
+  margin-top: var(--dl-space-space-halfunit);
+  align-items: flex-start;
+  margin-bottom: var(--dl-space-space-halfunit);
+  flex-direction: column;
+  justify-content: space-between;
+}
+.profile-icon6 {
+  width: 24px;
+  height: 24px;
+}
+@media(max-width: 991px) {
+  .profile-hero {
+    flex-direction: column;
+  }
+}
+@media(max-width: 767px) {
+  .profile-hero {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+}
+@media(max-width: 479px) {
+  .profile-hero {
+    padding-top: 32px;
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: 32px;
+  }
+}
+</style>
